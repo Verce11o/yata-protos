59b746e41407c8efbd21b77eab8b51e998de995a
Verce11o <taymas_rashitov@mail.ru>
2023-12-12 15:39:35 +0600

    adding get all tweets method

--- gen/go/tweets/tweets_grpc.pb.go
+++ gen/go/tweets/tweets_grpc.pb.go
@@ -19,10 +19,11 @@ import (
 const _ = grpc.SupportPackageIsVersion7
 
 const (
-	Tweets_CreateTweet_FullMethodName = "/tweets.Tweets/CreateTweet"
-	Tweets_GetTweet_FullMethodName    = "/tweets.Tweets/GetTweet"
-	Tweets_UpdateTweet_FullMethodName = "/tweets.Tweets/UpdateTweet"
-	Tweets_DeleteTweet_FullMethodName = "/tweets.Tweets/DeleteTweet"
+	Tweets_CreateTweet_FullMethodName  = "/tweets.Tweets/CreateTweet"
+	Tweets_GetTweet_FullMethodName     = "/tweets.Tweets/GetTweet"
+	Tweets_GetAllTweets_FullMethodName = "/tweets.Tweets/GetAllTweets"
+	Tweets_UpdateTweet_FullMethodName  = "/tweets.Tweets/UpdateTweet"
+	Tweets_DeleteTweet_FullMethodName  = "/tweets.Tweets/DeleteTweet"
 )
 
 // TweetsClient is the client API for Tweets service.
@@ -31,6 +32,7 @@ const (
 type TweetsClient interface {
 	CreateTweet(ctx context.Context, in *CreateTweetRequest, opts ...grpc.CallOption) (*CreateTweetResponse, error)
 	GetTweet(ctx context.Context, in *GetTweetRequest, opts ...grpc.CallOption) (*Tweet, error)
+	GetAllTweets(ctx context.Context, in *GetAllTweetsRequest, opts ...grpc.CallOption) (*GetAllTweetsResponse, error)
 	UpdateTweet(ctx context.Context, in *UpdateTweetRequest, opts ...grpc.CallOption) (*Tweet, error)
 	DeleteTweet(ctx context.Context, in *DeleteTweetRequest, opts ...grpc.CallOption) (*DeleteTweetResponse, error)
 }
@@ -61,6 +63,15 @@ func (c *tweetsClient) GetTweet(ctx context.Context, in *GetTweetRequest, opts .
 	return out, nil
 }
 
+func (c *tweetsClient) GetAllTweets(ctx context.Context, in *GetAllTweetsRequest, opts ...grpc.CallOption) (*GetAllTweetsResponse, error) {
+	out := new(GetAllTweetsResponse)
+	err := c.cc.Invoke(ctx, Tweets_GetAllTweets_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 func (c *tweetsClient) UpdateTweet(ctx context.Context, in *UpdateTweetRequest, opts ...grpc.CallOption) (*Tweet, error) {
 	out := new(Tweet)
 	err := c.cc.Invoke(ctx, Tweets_UpdateTweet_FullMethodName, in, out, opts...)
@@ -85,6 +96,7 @@ func (c *tweetsClient) DeleteTweet(ctx context.Context, in *DeleteTweetRequest, 
 type TweetsServer interface {
 	CreateTweet(context.Context, *CreateTweetRequest) (*CreateTweetResponse, error)
 	GetTweet(context.Context, *GetTweetRequest) (*Tweet, error)
+	GetAllTweets(context.Context, *GetAllTweetsRequest) (*GetAllTweetsResponse, error)
 	UpdateTweet(context.Context, *UpdateTweetRequest) (*Tweet, error)
 	DeleteTweet(context.Context, *DeleteTweetRequest) (*DeleteTweetResponse, error)
 	mustEmbedUnimplementedTweetsServer()
@@ -99,6 +111,9 @@ func (UnimplementedTweetsServer) CreateTweet(context.Context, *CreateTweetReques
 }
 func (UnimplementedTweetsServer) GetTweet(context.Context, *GetTweetRequest) (*Tweet, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetTweet not implemented")
+}
+func (UnimplementedTweetsServer) GetAllTweets(context.Context, *GetAllTweetsRequest) (*GetAllTweetsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetAllTweets not implemented")
 }
 func (UnimplementedTweetsServer) UpdateTweet(context.Context, *UpdateTweetRequest) (*Tweet, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method UpdateTweet not implemented")
@@ -155,6 +170,24 @@ func _Tweets_GetTweet_Handler(srv interface{}, ctx context.Context, dec func(int
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Tweets_GetAllTweets_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetAllTweetsRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(TweetsServer).GetAllTweets(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: Tweets_GetAllTweets_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(TweetsServer).GetAllTweets(ctx, req.(*GetAllTweetsRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 func _Tweets_UpdateTweet_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(UpdateTweetRequest)
 	if err := dec(in); err != nil {
@@ -205,6 +238,10 @@ var Tweets_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetTweet",
 			Handler:    _Tweets_GetTweet_Handler,
+		},
+		{
+			MethodName: "GetAllTweets",
+			Handler:    _Tweets_GetAllTweets_Handler,
 		},
 		{
 			MethodName: "UpdateTweet",
